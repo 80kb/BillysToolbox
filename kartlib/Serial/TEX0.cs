10e7caa71187ed147a7e5b990bb0b135659ffb54
J Henning <jonathanchenning@outlook.com>
2024-08-06 14:04:11 -0400

    Begin work on TEX0.

---
+++ kartlib/Serial/TEX0.cs
@@ -0,0 +1,15 @@
+﻿namespace kartlib.Serial
+{
+    public class TEX0
+    {
+        public class _Header
+        {
+            public UInt32 Magic;
+            public UInt32 SubFileLength;
+            public UInt32 Version;
+            public Int32 OffsetToParent;
+            public Int32[] SectionOffsets;
+            public Int32 NameOffset;
+        }
+    }
+}
