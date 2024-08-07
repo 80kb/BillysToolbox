ee7b75f0f2eaa425d41d1b0aa3be776ffdccdde4
J Henning <jonathanchenning@outlook.com>
2024-08-06 22:19:22 -0400

    Added BDOF as support file

--- BillysToolbox/MainForm.cs
+++ BillysToolbox/MainForm.cs
@@ -14,6 +14,7 @@ namespace BillysToolbox
             { "BMM Files (*.bmm)", "*.bmm" },
             { "KMP Files (*.kmp)", "*.kmp" },
             { "BLIGHT Files (*.blight)", "*.blight" },
+            { "BDOF Files (*.bdof_", "*.bdof" },
             { "KCL Files (*.kcl)", "*.kcl" },
             { "All Files (*.*)", "*.*" },
         };
